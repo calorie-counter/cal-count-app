cade2e8bb732d6dce6cee0106a916630d12d2c78
Drew Nauman <din3@cornell.edu>
2016-07-08 08:54:17 -0600

    debug progress logic

--- public/components/track/track.js
+++ public/components/track/track.js
@@ -6,28 +6,37 @@ app.controller("TrackController", ["$scope", "FoodService", "UserService", funct
     $scope.tabName = "day";
     $scope.i = -1;
 
+    var foodList = $scope.foodService.foodList;
+    var getProgress = function () {
+        var kCal = 0;
+        for (var i = 0; i < foodList.length; i++) {
+            kCal += foodList[i].calories;
+        }
+        return kCal
+    }
+    var goal = UserService.currentUser.goal;
+    var now = getProgress();
+
+    function setProgress() {
+        $scope.progress = {
+            goal: goal,
+            now: now,
+            percent: Math.round(now / goal * 100)
+        }
+    }
+    setProgress();
+    console.log($scope.progress);
+
     $scope.checkDate = function () {
         $scope.foodService.getFoods($scope.tabName);
+        setProgress();
+        console.log($scope.progress);
     }
 
     $scope.set = function (index) {
-        var foodList = $scope.foodService.foodList;
-        var getProgress = function () {
-            var kCal = 0;
-            for (var i = 0; i < foodList.length; i++) {
-                kCal += foodList[i].calories;
-            }
-            return kCal
-        }
-        var goal = UserService.currentUser.goal;
-        var now = getProgress();
-        
         $scope.i = index;
-        $scope.progress = {
-            goal : goal,
-            now : now,
-            percent : Math.round(now / goal *100)
-        }
+        setProgress();
+        console.log($scope.progress);
     }
 
 }]);
